b240065855c02eb3f1a7db8bfb9d46b59bbbb387
Petek Savaş Hamitbeyli <38165351+petekgithub@users.noreply.github.com>
2022-05-09 16:16:07 +0300

    localization

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react'
+import {IntlProvider, FormattedMessage, FormattedNumber} from 'react-intl'
 import './App.css';
 
+const messages = {
+  'tr-TR': {
+    title: 'Yaz',
+    description: '{count} yeni mesajınız var'
+  },
+  'en-US' : {
+    title: 'Summer',
+    description: 'You have {count} new mesages'
+
+  }
+}
+
+
 function App() {
+
+const isLocal = localStorage.getItem("locale")
+const defaultLocale = isLocal ? isLocal : navigator.language
+
+const [locale, setLocale] = useState(defaultLocale)
+
+// when language changed I can write on localstorage(with useEffect) and it can be changed default 
+useEffect(() => {
+  localStorage.setItem("locale",locale)
+}, [locale])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <IntlProvider locale= {locale} messages = {messages[locale]}>
+      <FormattedMessage id="title"/>
+      
+      <p> <FormattedMessage 
+      id="description"
+      values={{count:3}}
+      
+      /> </p>
+      
+      <br />
+      <br />
+      <button onClick={() => setLocale('tr-TR')}>TR</button>
+      <button onClick={() => setLocale('en-US')}>EN</button>
+     </IntlProvider>
+     
     </div>
   );
 }
